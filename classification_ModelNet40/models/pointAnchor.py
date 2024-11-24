63ba69ef091e52dfb10fc47f3492c1b6d74ac01f
alfeak <lensurplus@gmail.com>
2024-11-24 18:29:47 +0800

    adjust model to fullset with local_grouper

--- classification_ModelNet40/models/pointAnchor.py
+++ classification_ModelNet40/models/pointAnchor.py
@@ -1,39 +1,49 @@
 import torch
 import torch.nn as nn
-from .utils import pointBase_conv,gen_curve
+from .utils import PointConv, PointMaxPool, PointFullAgreggation
 
 class PointAnchorNet(nn.Module):
     def __init__(self):
         super(PointAnchorNet, self).__init__()
-        self.knn = 24
-
-        self.layer1 = pointBase_conv(3, 64,self.knn,3)
-        self.layer2 = pointBase_conv(64,128,6,2,False)
-        self.layer3 = pointBase_conv(128,256,6,1,False)
-
+        knn = 24
+        dilation = 1
+        self.conv = nn.Sequential(
+            nn.Conv1d(3, 64, kernel_size=1, bias=False),
+            nn.BatchNorm1d(64),
+            nn.ReLU(inplace=True),
+        )
+        self.convlayer = nn.Sequential(
+            # PointMaxPool(knn,2,dilation),
+            PointConv(64,128,knn,2,dilation),
+            PointConv(128,256,knn,2,dilation),
+            PointConv(256,512,knn,2,dilation),
+            PointConv(512,1024,knn,2,dilation),
+            # PointFullAgreggation()
+        )
+        self.classifier = nn.Linear(1024,40)
         self.pool = nn.AdaptiveMaxPool1d(1)
-        self.fc = nn.Linear(256, 40)
-        self.batch_list = dict()
-
+        # self.classifier = nn.Linear(1024, 40)
+        # self.classifier = nn.Sequential(
+        #     nn.Linear(1024, 512),
+        #     nn.BatchNorm1d(512),
+        #     nn.ReLU(),
+        #     nn.Dropout(0.5),
+        #     nn.Linear(512, 256),
+        #     nn.BatchNorm1d(256),
+        #     nn.ReLU(),
+        #     nn.Dropout(0.5),
+        #     nn.Linear(256, 40)
+        # )
     def forward(self, xyz):
-        B, _, _ = xyz.shape
-        if B not in self.batch_list.keys():
-            self.batch_list[B] = [gen_curve(B,1,xyz.device),\
-                                  gen_curve(B,2,xyz.device),\
-                                  gen_curve(B,3,xyz.device)]
-
-        xyz = self.layer1(xyz, self.batch_list[B])
-        # print("xyz: ",xyz.shape)
-        xyz = self.layer2(xyz, self.batch_list[B])
-        xyz = self.layer3(xyz, self.batch_list[B])
-        x = self.pool(xyz).squeeze()
-        x = self.fc(x)
-
-        return x
+        points = self.conv(xyz.permute(0,2,1))
+        points,xyz = self.convlayer((points,xyz))
+        points = self.pool(points).squeeze(-1)
+        points = self.classifier(points)
+        return points
+    
 if __name__ == '__main__':
-    data = torch.rand(2, 3, 1024)
+    data = torch.rand(2, 1024, 3).cuda()
     print("===> testing pointMLP ...")
-    model = PointAnchorNet()
+    model = PointAnchorNet().cuda()
     out = model(data)
     print(out.shape)
-
--- classification_ModelNet40/models/utils.py
+++ classification_ModelNet40/models/utils.py
@@ -1,22 +1,12 @@
+from hmac import new
+from re import S
+import re
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from time import time
 import numpy as np
-import math
-from hilbert import decode
-
-def timeit(tag, t):
-    print("{}: {}s".format(tag, time() - t))
-    return time()
-
-def pc_normalize(pc):
-    l = pc.shape[0]
-    centroid = np.mean(pc, axis=0)
-    pc = pc - centroid
-    m = np.max(np.sqrt(np.sum(pc**2, axis=1)))
-    pc = pc / m
-    return pc
+from pointnet2_ops import pointnet2_utils
 
 def square_distance(src, dst):
     """
@@ -40,8 +30,6 @@ def square_distance(src, dst):
     dist += torch.sum(src ** 2, -1).view(B, N, 1)
     dist += torch.sum(dst ** 2, -1).view(B, 1, M)
     return dist
-
-
 
 def index_points(points, idx):
     """
@@ -75,139 +63,100 @@ def knn_point(nsample, xyz, new_xyz):
     _, group_idx = torch.topk(sqrdists, nsample, dim=-1, largest=False, sorted=True)
     return group_idx
 
-def gen_curve(batch_num, num_bits,device):
-    # Number of dimensions.
-    num_dims = 3
-    # The maximum Hilbert integer.
-    max_h = 2**(num_bits*num_dims)
-  # Generate a sequence of Hilbert integers.
-    hilberts = np.arange(max_h)
-    # Compute the 2-dimensional locations.
-    locs = decode(hilberts, num_dims, num_bits)
-    locs = locs.astype(np.float32) / (2**num_bits - 1) *2 -1
-    anchor_pc = torch.tensor(locs).to(device).unsqueeze(0).repeat(batch_num,1,1)
-
-    return anchor_pc
-
-def gen_anchor_pc1d(batch_num,anchor_pc_num,range,device):
-    min_,max_ = range
-    anchor_pc = torch.linspace(min_,max_,anchor_pc_num).to(device).unsqueeze(-1).repeat(batch_num,1,1)
-    zero_padding = torch.zeros(anchor_pc_num).to(device).unsqueeze(-1).repeat(batch_num,1,1)
-    anchor_pc = torch.cat([anchor_pc,zero_padding,zero_padding],dim=-1)
-    return anchor_pc
-
-class ChannelAttention1d(nn.Module):
-    def __init__(self, in_planes, ratio=16):
-        super(ChannelAttention1d, self).__init__()
-        self.avg_pool = nn.AdaptiveAvgPool1d(1)
-        self.max_pool = nn.AdaptiveMaxPool1d(1)
-           
-        self.fc = nn.Sequential(nn.Conv1d(in_planes, in_planes // 16, 1, bias=False),
-                               nn.ReLU(),
-                               nn.Conv1d(in_planes // 16, in_planes, 1, bias=False))
-        self.sigmoid = nn.Sigmoid()
-
-    def forward(self, x):
-        x0 = x
-        avg_out = self.fc(self.avg_pool(x))
-        max_out = self.fc(self.max_pool(x))
-        out = avg_out + max_out
-        return self.sigmoid(out) * x0
-
-class SpatialAttention1d(nn.Module):
-    def __init__(self, kernel_size=7):
-        super(SpatialAttention1d, self).__init__()
-
-        self.conv1 = nn.Conv1d(2, 1, kernel_size, padding=kernel_size//2, bias=False)
-        self.sigmoid = nn.Sigmoid()
-
-    def forward(self, x):
-        x0 = x
-        avg_out = torch.mean(x, dim=1, keepdim=True)
-        max_out, _ = torch.max(x, dim=1, keepdim=True)
-        x = torch.cat([avg_out, max_out], dim=1)
-        x = self.conv1(x)
-        return self.sigmoid(x) * x0
-
-class pointBase_embedding(nn.Module):
-    def __init__(self,in_channel,out_channel, knn):
-        super(pointBase_embedding,self).__init__()
-        self.conv = nn.Sequential(
-            nn.Conv2d(in_channel*2,out_channel,1),
-            nn.BatchNorm2d(out_channel),
-            nn.ReLU(),
-            nn.MaxPool2d((1,knn))
-        )
-        self.affine_alpha = nn.Parameter(torch.ones([1,1,1, in_channel]))
-        self.affine_beta = nn.Parameter(torch.zeros([1, 1, 1, in_channel]))
-
-    def forward(self,xyz):
-        B, N ,K, C = xyz.shape
-        mean = torch.mean(xyz, dim=2, keepdim=True)
-        std = torch.std((xyz-mean).reshape(B,-1),dim=-1,keepdim=True).unsqueeze(dim=-1).unsqueeze(dim=-1)
-        new_xyz = (xyz-mean)/(std + 1e-5)
-        new_xyz = self.affine_alpha*new_xyz + self.affine_beta
-        new_xyz = torch.cat((xyz,new_xyz),dim=-1)
-        new_xyz = new_xyz.permute(0,3,1,2)
-        new_xyz = self.conv(new_xyz).squeeze()
-        return new_xyz
-
-def arrange_pc(xyz,anchor_pc,order,knn,is_first=True):
-    # print(order)
-    xyz = xyz.permute(0,2,1)
-    if is_first:
-        # print(xyz.shape,anchor_pc[order-1].shape)
-        idx = knn_point(knn,xyz,anchor_pc[order-1])
-        # new_xyz = index_points(xyz.permute(0,2,1), idx) #.permute(0,3,1,2)
-    else:
-        # print(xyz.shape,anchor_pc[order].shape,anchor_pc[order-1].shape)
-        idx = knn_point(knn,anchor_pc[order],anchor_pc[order-1])
-    new_xyz = index_points(xyz, idx) #.permute(0,3,1,2)
-    return new_xyz
-
-class pointBase_conv(nn.Module):
-    def __init__(self,in_channel,out_channel,knn=24,order=3,is_first=True,stride=8):
-        super(pointBase_conv,self).__init__()
-        self.order = order
-        self.is_first = is_first
+class PointMaxPool(nn.Module):
+    def __init__(self,knn=1,stride=1,dilation=1):
+        super(PointMaxPool,self).__init__()
         self.knn = knn
-        self.conv = nn.Sequential(
-            nn.Conv2d(in_channel*2,out_channel,1),
-            nn.BatchNorm2d(out_channel),
-            nn.ReLU(),
-            nn.MaxPool2d((1,knn)),
-        )
-        self.conv1 = nn.Sequential(
-            nn.Conv1d(out_channel,out_channel,3,1,1),
-            nn.BatchNorm1d(out_channel),
-            nn.ReLU(),
-            ChannelAttention1d(out_channel),
-            SpatialAttention1d(),
-        )
-        self.affine_alpha = nn.Parameter(torch.ones([1,1,1, in_channel]))
-        self.affine_beta = nn.Parameter(torch.zeros([1, 1, 1, in_channel]))
-        
-    def forward(self,xyz,anchor_pc):
-        new_xyz = arrange_pc(xyz,anchor_pc,self.order,self.knn,self.is_first)
-        B, N ,K, C = new_xyz.shape
-        mean = torch.mean(new_xyz, dim=2, keepdim=True)
-        std = torch.std((new_xyz-mean).reshape(B,-1),dim=-1,keepdim=True).unsqueeze(dim=-1).unsqueeze(dim=-1)
-        normalized_xyz = (new_xyz-mean)/(std + 1e-5)
-        normalized_xyz = self.affine_alpha*normalized_xyz + self.affine_beta
-        new_xyz = torch.cat((new_xyz,normalized_xyz),dim=-1)
-        new_xyz = new_xyz.permute(0,3,1,2)
-        xyz = self.conv(new_xyz).squeeze()
-        xyz = xyz + self.conv1(xyz)
+        self.stride = stride
+        self.dilation = dilation
+        self.pool = nn.MaxPool2d((1,self.knn))
 
-        return xyz
+    def forward(self,x):
+        points,xyz = x
+        points = points.permute(0,2,1)
+        B, N, C = xyz.shape
+        fps_idx = pointnet2_utils.furthest_point_sample(xyz, N//self.stride).long()
+        sampled_xyz = index_points(xyz, fps_idx)
+        sampled_points = index_points(points, fps_idx)
+
+        idx = knn_point(self.knn * self.dilation, xyz, sampled_xyz)[:, :, ::self.dilation]
+        grouped_points = index_points(points, idx) - sampled_points.unsqueeze(-2).repeat(1,1,self.knn,1)  # [B, npoint, nsample, C]
+        new_points = self.pool(grouped_points.permute(0,3,1,2)).squeeze(-1) + sampled_points.permute(0,2,1)  # [B, C, npoint, nsample]
+        return (new_points, sampled_xyz)
+
+class PointFullAgreggation(nn.Module):
+    def __init__(self):
+        super(PointFullAgreggation,self).__init__()
+        self.pool = nn.AdaptiveMaxPool1d(1)
+    def forward(self,x):
+        points,xyz = x
+        points = points.permute(0,2,1)
+        B, N, C = xyz.shape
+        idx = knn_point(N, xyz, xyz)
+        grouped_points = index_points(points, idx).permute(0,3,1,2)  # [B, C, N, nsample]
+        new_points = F.max_pool2d(grouped_points, kernel_size=(1,N)).squeeze(-1)  # [B, C, N]
+        new_points = self.pool(new_points).squeeze(-1)  # [B, C]
+        return (new_points, xyz)
+
+class PointConv(nn.Module):
+    def __init__(self,in_channel,out_channel,knn=1,stride=1,dilation=1):
+        super(PointConv,self).__init__()
+        self.knn = knn
+        self.stride = stride
+        self.dilation = dilation
+        self.in_channel = in_channel
+        self.out_channel = out_channel
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channel,out_channel,kernel_size=1,bias=False),
+            nn.BatchNorm2d(out_channel),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channel,out_channel,kernel_size=1,bias=False),
+            nn.BatchNorm2d(out_channel),
+            nn.MaxPool2d((1,self.knn))
+        )
+        self.affine_alpha = nn.Parameter(torch.ones([1,1,1,in_channel]))
+        self.affine_beta = nn.Parameter(torch.zeros([1, 1, 1, in_channel]))
+        self.conv1 =  nn.Sequential(
+            nn.Conv2d(out_channel,out_channel,kernel_size=1,bias=False),
+            nn.BatchNorm2d(out_channel),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channel,out_channel,kernel_size=1,bias=False),
+            nn.BatchNorm2d(out_channel),
+            nn.MaxPool2d((1,self.knn))
+        )
+        if in_channel == out_channel:
+            self.identity = nn.Sequential()
+        else:
+            self.identity = nn.Sequential(
+                nn.Conv1d(in_channel,out_channel,kernel_size=1,bias=False),
+                nn.BatchNorm1d(out_channel),
+        )
+        # self.pool = nn.MaxPool2d((1,self.knn))
+
+    def forward(self,x):
+        points,xyz = x
+        B, N, C = xyz.shape
+        xyz = xyz.contiguous() 
+        fps_idx = pointnet2_utils.furthest_point_sample(xyz, N//self.stride).long()
+        sampled_xyz = index_points(xyz, fps_idx)
+        sampled_points = index_points(points.permute(0,2,1),fps_idx).permute(0,2,1)
+        
+        idx = knn_point(self.knn * self.dilation, xyz, sampled_xyz)[:, :, ::self.dilation]
+        grouped_points = index_points(points.permute(0,2,1), idx) - sampled_points.permute(0,2,1).unsqueeze(-2).repeat(1,1,self.knn,1)
+        std = torch.std((grouped_points).reshape(B,-1),dim=-1,keepdim=True).unsqueeze(dim=-1).unsqueeze(dim=-1)
+        grouped_points = (grouped_points)/(std + 1e-5)
+        grouped_points = self.affine_alpha*grouped_points + self.affine_beta
+        grouped_points = self.conv(grouped_points.permute(0,3,1,2)).squeeze(-1)
+        new_points = F.relu(grouped_points + self.identity(sampled_points))
+        
+        idx = knn_point(self.knn * self.dilation, sampled_xyz, sampled_xyz)[:, :, ::self.dilation]
+        grouped_points = index_points(new_points.permute(0,2,1), idx) - new_points.permute(0,2,1).unsqueeze(-2).repeat(1,1,self.knn,1)
+        grouped_points = self.conv1(grouped_points.permute(0,3,1,2)).squeeze(-1)
+        new_points = F.relu(grouped_points + new_points)
+        
+        return (new_points,sampled_xyz)
+        # return new_points
 
 if __name__ == "__main__":
-    # pass
-    xyz = torch.randn(1, 1024,3)
-    high_xyz = torch.randn(1, 1024, 6)
-    new_xyz = torch.randn(1,24,3)
-    new_xyz = arrange_pc(high_xyz,xyz,new_xyz,24)
-    print(new_xyz.shape)
-    # curve = gen_curve(2,3,xyz.device)
-    # print(curve.shape)
-    # print(curve[0][:10])
+    pass
+    
