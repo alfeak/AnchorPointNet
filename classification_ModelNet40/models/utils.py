3eaa39366def096b8878660205a5f6101f27141f
alfeak <lensurplus@gmail.com>
2024-11-27 14:04:04 +0000

    bn_pointconv_conv1

--- classification_ModelNet40/models/utils.py
+++ classification_ModelNet40/models/utils.py
@@ -156,14 +156,12 @@ class PointConv(nn.Module):
         self.conv = nn.Sequential(
             nn.Conv2d(in_channel,out_channel,kernel_size=1,bias=False),
             nn.BatchNorm2d(out_channel),
+            nn.ReLU(inplace=True),
             nn.MaxPool2d((1,self.knn)),
         )
-        if in_channel == out_channel:
-            self.identity = nn.Sequential()
-        else:
-            self.identity = nn.Sequential(
-                nn.Conv1d(in_channel,out_channel,kernel_size=1,bias=False),
-                nn.BatchNorm1d(out_channel),
+        self.conv1 = nn.Sequential(
+            nn.Conv1d(out_channel,out_channel,kernel_size=1,bias=False),
+            nn.BatchNorm1d(out_channel),
         )
 
     def forward(self,x):
@@ -180,7 +178,7 @@ class PointConv(nn.Module):
         grouped_points = index_points(points.permute(0,2,1),idx).permute(0,3,1,2)
         new_points = self.bn(grouped_points) + sampled_points.unsqueeze(-1).repeat(1,1,1,self.knn)
         new_points = self.conv(new_points).squeeze(-1)
-        new_points = F.relu(new_points + self.identity(sampled_points))
+        new_points = F.relu(self.conv1(new_points) + new_points)
         return (new_points,sampled_xyz)
 
 class PointResConv(nn.Module):
