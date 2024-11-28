87f7626152ff4913b0b56cc059cdca3cb8709ca9
alfeak <lensurplus@gmail.com>
2024-11-28 08:08:49 +0000

    res_connection_concat

--- classification_ModelNet40/models/utils.py
+++ classification_ModelNet40/models/utils.py
@@ -102,52 +102,41 @@ class PointConv(nn.Module):
             nn.ReLU(inplace=True),
             nn.MaxPool2d((1,self.knn)),
         )
+        self.pool = nn.MaxPool2d((1,self.knn))
         self.bn1 = nn.BatchNorm2d(2)
         self.conv1 = nn.Sequential(
             nn.Conv2d(2,1,kernel_size=1,bias=False),
             nn.MaxPool2d((1,self.knn)),
         )
         self.sigmoid = nn.Sigmoid()
-        if in_channel == out_channel:
-            self.identity = nn.Sequential()
-        else:
-            self.identity = nn.Sequential(
-                nn.Conv1d(in_channel,out_channel,kernel_size=1,bias=False),
-                nn.BatchNorm1d(out_channel),
-        )
-        self.conv2 = nn.Sequential(
-            nn.Conv1d(out_channel,out_channel,kernel_size=1,bias=False),
-            nn.BatchNorm1d(out_channel),
-            nn.ReLU(inplace=True),
-            nn.Conv1d(out_channel,out_channel,kernel_size=1,bias=False),
-            nn.BatchNorm1d(out_channel),
-        )
 
     def forward(self,x):
         points,xyz = x
         B, N, C = xyz.shape
         xyz = xyz.contiguous() 
         fps_idx = pointnet2_utils.furthest_point_sample(xyz, N//self.stride).long()
+        # fps_idx = sort_sample(xyz,self.stride)
 
         sampled_xyz = index_points(xyz, fps_idx)
         sampled_points = index_points(points.permute(0,2,1),fps_idx).permute(0,2,1)
         
         idx = knn_point(self.knn * self.dilation, xyz, sampled_xyz)[:, :, ::self.dilation]
         grouped_points = index_points(points.permute(0,2,1),idx).permute(0,3,1,2) 
-        new_points = self.bn(grouped_points - grouped_points[:,:,:,0].unsqueeze(-1)) + grouped_points[:,:,:,0].unsqueeze(-1)
-        new_points = self.conv(new_points).squeeze(-1)
+        # new_points = self.bn(grouped_points - grouped_points[:,:,:,0].unsqueeze(-1)) + grouped_points[:,:,:,0].unsqueeze(-1)
+        new_points = self.conv(grouped_points).squeeze(-1)
+        identity = torch.cat([self.pool(grouped_points).squeeze(-1),sampled_points],dim=1)
                 
         idx = knn_point(self.knn * self.dilation, sampled_xyz, sampled_xyz)[:, :, ::self.dilation]
         grouped_points = index_points(new_points.permute(0,2,1),idx).permute(0,3,1,2) 
         max_pooled = torch.max(grouped_points, dim=1, keepdim=True)[0]  # [b, 1, n, k]
         avg_pooled = torch.mean(grouped_points, dim=1, keepdim=True)    # [b, 1, n, k]
         spatial_attention = torch.cat([max_pooled, avg_pooled], dim=1)  # [b, 2, n, k]
-        spatial_attention = self.bn1(spatial_attention - spatial_attention[:,:,:,0].unsqueeze(-1)) + spatial_attention[:,:,:,0].unsqueeze(-1)
+        # spatial_attention = self.bn1(spatial_attention - spatial_attention[:,:,:,0].unsqueeze(-1)) + spatial_attention[:,:,:,0].unsqueeze(-1)
         spatial_attention = self.conv1(spatial_attention)
         spatial_attention = self.sigmoid(spatial_attention).squeeze(-1)
         new_points = new_points*spatial_attention
-        new_points = F.relu(new_points + self.identity(sampled_points))
-        new_points = F.relu(new_points + self.conv2(new_points))
+        
+        new_points = new_points + identity
         return (new_points,sampled_xyz)
 
 if __name__ == "__main__":
