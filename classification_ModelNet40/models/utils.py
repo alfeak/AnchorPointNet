9d7c51da85d4b7dca3098f2407f31f94ea342690
alfeak <lensurplus@gmail.com>
2024-11-29 20:31:14 +0000

    bn(g)+a_concat

--- classification_ModelNet40/models/utils.py
+++ classification_ModelNet40/models/utils.py
@@ -124,17 +124,12 @@ class PointConv(nn.Module):
         self.out_channel = out_channel
         self.bn = nn.BatchNorm2d(in_channel)
         self.conv = nn.Sequential(
-            nn.Conv2d(in_channel,out_channel,kernel_size=1,bias=False),
+            nn.Conv2d(in_channel*2,out_channel,kernel_size=1,bias=False),
             nn.BatchNorm2d(out_channel),
             nn.ReLU(inplace=True),
             nn.MaxPool2d((1,self.knn)),
         )
-        self.bn1 = nn.BatchNorm2d(out_channel)
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(out_channel,out_channel,kernel_size=1,bias=False),
-            nn.BatchNorm2d(out_channel),
-            nn.MaxPool2d((1,self.knn)),
-        )
+        
         if in_channel == out_channel:
             self.identity = nn.Sequential()
         else:
@@ -154,14 +149,9 @@ class PointConv(nn.Module):
         
         idx = knn_point(self.knn * self.dilation, xyz, sampled_xyz)[:, :, ::self.dilation]
         grouped_points = index_points(points.permute(0,2,1),idx).permute(0,3,1,2) 
-        grouped_points = self.bn(grouped_points)# + grouped_points[:,:,:,0].unsqueeze(-1)
+        grouped_points = self.bn(grouped_points) + grouped_points[:,:,:,0].unsqueeze(-1)
+        grouped_points = torch.cat([grouped_points,sampled_points.unsqueeze(-1).repeat(1,1,1,self.knn)],dim=1)
         new_points = self.conv(grouped_points).squeeze(-1)
-
-        idx = knn_point(self.knn * self.dilation, sampled_xyz, sampled_xyz)[:, :, ::self.dilation]
-        grouped_points = index_points(new_points.permute(0,2,1),idx).permute(0,3,1,2) 
-        grouped_points = self.bn1(grouped_points)# + grouped_points[:,:,:,0].unsqueeze(-1)
-        new_points = self.conv1(grouped_points).squeeze(-1)
-
         new_points = F.relu(new_points + self.identity(sampled_points))
         return (new_points,sampled_xyz)
 
