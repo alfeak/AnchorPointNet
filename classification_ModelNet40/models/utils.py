0c802cfe2305ab2c5e0e09924f46e50359e86891
alfeak <lensurplus@gmail.com>
2024-12-01 05:47:56 +0000

    conv1_resc

--- classification_ModelNet40/models/utils.py
+++ classification_ModelNet40/models/utils.py
@@ -163,13 +163,13 @@ class PointConv(nn.Module):
         
         idx = knn_point(self.knn * self.dilation, xyz, sampled_xyz)[:, :, ::self.dilation]
         grouped_points = index_points(points.permute(0,2,1),idx).permute(0,3,1,2) 
-        grouped_points = self.bn(grouped_points) + grouped_points[:,:,:,0].unsqueeze(-1)
+        # grouped_points = self.bn(grouped_points) + grouped_points[:,:,:,0].unsqueeze(-1)
         grouped_points = self.conv(grouped_points).squeeze(-1)
         new_points = F.relu(grouped_points + self.identity(sampled_points))
 
         idx = knn_point(self.knn * self.dilation, sampled_xyz, sampled_xyz)[:, :, ::self.dilation]
         grouped_points = index_points(new_points.permute(0,2,1),idx).permute(0,3,1,2) 
-        grouped_points = self.bn1(grouped_points) + grouped_points[:,:,:,0].unsqueeze(-1)
+        # grouped_points = self.bn1(grouped_points) + grouped_points[:,:,:,0].unsqueeze(-1)
         grouped_points = self.conv1(grouped_points).squeeze(-1)
         new_points = F.relu(grouped_points + self.identity1(new_points) + new_points)
         return (new_points,sampled_xyz)
