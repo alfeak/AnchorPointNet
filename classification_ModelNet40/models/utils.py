5ab2af97c68372b543874ec50137ba29185ca59b
alfeak <lensurplus@gmail.com>
2024-12-02 07:38:01 +0000

    conv1_res2

--- classification_ModelNet40/models/utils.py
+++ classification_ModelNet40/models/utils.py
@@ -151,12 +151,12 @@ class PointConv(nn.Module):
         sampled_points = index_points(points.permute(0,2,1),fps_idx).permute(0,2,1)
         
         points = self.conv(points)
-        idx = knn_point(self.knn * self.dilation, xyz, sampled_xyz)[:, :, ::self.dilation]
+        idx = knn_point(self.knn * self.dilation, xyz, xyz)[:, :, ::self.dilation]
         grouped_points = index_points(points.permute(0,2,1),idx).permute(0,3,1,2) 
         new_points = self.pool(grouped_points).squeeze(-1)
         
         points = self.conv1(new_points)
-        idx = knn_point(self.knn * self.dilation, sampled_xyz, sampled_xyz)[:, :, ::self.dilation]
+        idx = knn_point(self.knn * self.dilation, xyz, sampled_xyz)[:, :, ::self.dilation]
         grouped_points = index_points(points.permute(0,2,1),idx).permute(0,3,1,2)
         new_points = self.pool(grouped_points).squeeze(-1)
         new_points = F.relu(new_points + self.identity(sampled_points))
