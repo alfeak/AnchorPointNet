21916d9ea25d4863d4e4a11573570f79a4520187
alfeak <lensurplus@gmail.com>
2024-12-05 05:54:48 +0000

    baseline

--- classification_ModelNet40/models/pointAnchor.py
+++ classification_ModelNet40/models/pointAnchor.py
@@ -7,23 +7,24 @@ class PointAnchorNet(nn.Module):
         super(PointAnchorNet, self).__init__()
         knn = knn
         dilation = dilation
-        self.conv = nn.Sequential(
-          nn.Conv1d(3, 64, kernel_size=1, bias=False),
-          nn.BatchNorm1d(64),
-          nn.ReLU(inplace=True),
-        )
+        # self.conv = nn.Sequential(
+        #   nn.Conv1d(3, 64, kernel_size=1, bias=False),
+        #   nn.BatchNorm1d(64),
+        #   nn.ReLU(inplace=True),
+        # )
         self.convlayer = nn.Sequential(
-            PointConv(64,128,knn,2,dilation),
-            PointConv(128,256,knn,2,dilation),
-            PointConv(256,512,knn,2,dilation),
-            PointConv(512,1024,knn,2,dilation),
+          PointConv(3,64,knn,2,dilation),
+          PointConv(64,128,knn,2,dilation),
+          PointConv(128,256,knn,2,dilation),
+          PointConv(256,512,knn,2,dilation),
+          PointConv(512,1024,knn,2,dilation),
         )
         self.pool = nn.AdaptiveMaxPool1d(1)
         self.classifier = nn.Linear(1024, 40)
     def forward(self, xyz):
-        points = self.conv(xyz.permute(0,2,1))
-        points,xyz = self.convlayer((points,xyz))
-        # points,xyz = self.convlayer((xyz.permute(0,2,1),xyz))
+        # points = self.conv(xyz.permute(0,2,1))
+        # points,xyz = self.convlayer((points,xyz))
+        points,xyz = self.convlayer((xyz.permute(0,2,1),xyz))
         points = self.pool(points).squeeze(-1)
         points = self.classifier(points)
         return points
