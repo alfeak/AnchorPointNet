4dc4c132ca4e98241d003e722af3115e7c3b7351
alfeak <lensurplus@gmail.com>
2024-12-07 08:36:16 +0000

    add_v100_compile

--- classification_ModelNet40/models/utils.py
+++ classification_ModelNet40/models/utils.py
@@ -159,21 +159,26 @@ class PointResConv(nn.Module):
         self.dilation = dilation
         self.in_channel = in_channel
         self.out_channel = out_channel
-        self.conv = nn.Conv1d(in_channel,out_channel,kernel_size=1,bias=False)
-        self.bn = nn.BatchNorm1d(out_channel)
-        self.conv1 = nn.Conv1d(out_channel,out_channel,kernel_size=1,bias=False)
-        self.bn1 = nn.BatchNorm1d(out_channel)
-        if in_channel == out_channel:
-            self.identity = nn.Sequential()
-        else:
-            self.identity = nn.Sequential(
-              nn.Conv1d(in_channel,out_channel,kernel_size=1,bias=False),
-              nn.BatchNorm1d(out_channel),
-            )
-        self.identity1 = nn.Sequential(
-          nn.Conv1d(out_channel,out_channel,kernel_size=1,bias=False),
-          nn.BatchNorm1d(out_channel),
+        self.bn2d = nn.BatchNorm2d(in_channel)
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channel,out_channel,kernel_size=1,bias=False),
+            nn.MaxPool2d((1,knn)),
         )
+        self.bn1d = nn.BatchNorm1d(out_channel)
+
+        self.bn2d1 = nn.BatchNorm2d(out_channel)
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(out_channel,out_channel,kernel_size=1,bias=False),
+            nn.MaxPool2d((1,knn)),
+        )
+        self.bn1d1 = nn.BatchNorm1d(out_channel)
+        # if in_channel == out_channel:
+        #     self.identity = nn.Sequential()
+        # else:
+        #     self.identity = nn.Sequential(
+        #         nn.Conv1d(in_channel,out_channel,kernel_size=1,bias=False),
+        #         nn.BatchNorm1d(out_channel),
+        #     )
         
     def forward(self,x):
         points,xyz = x
@@ -183,21 +188,21 @@ class PointResConv(nn.Module):
         sampled_xyz = index_points(xyz, fps_idx)
         sampled_points = index_points(points.permute(0,2,1),fps_idx).permute(0,2,1)
         
-        points = self.conv(points)
         idx = knn_point(self.knn * self.dilation, xyz, sampled_xyz)[:, :, ::self.dilation]
         grouped_points = index_points(points.permute(0,2,1), idx)
         grouped_points = grouped_points.permute(0,3,1,2)
-        grouped_points = torch.sum(grouped_points,dim=-1)
-        grouped_points = self.bn(grouped_points)
-        new_points = F.relu(grouped_points + self.identity(sampled_points))
-
-        points = self.conv1(new_points)
+        grouped_points = self.bn2d(grouped_points) + sampled_points.unsqueeze(-1)
+        grouped_points = self.conv(grouped_points).squeeze(-1)
+        grouped_points = self.bn1d(grouped_points)
+        new_points = F.relu(grouped_points)
+        
         idx = knn_point(self.knn * self.dilation, sampled_xyz, sampled_xyz)[:, :, ::self.dilation]
-        grouped_points = index_points(points.permute(0,2,1), idx)
+        grouped_points = index_points(new_points.permute(0,2,1), idx)
         grouped_points = grouped_points.permute(0,3,1,2)
-        grouped_points = torch.sum(grouped_points,dim=-1)
-        grouped_points = self.bn1(grouped_points)
-        new_points = F.relu(grouped_points + self.identity1(new_points) + new_points)
+        grouped_points = self.bn2d1(grouped_points) + new_points.unsqueeze(-1)
+        grouped_points = self.conv1(grouped_points).squeeze(-1)
+        grouped_points = self.bn1d1(grouped_points)
+        new_points = F.relu(grouped_points)
 
         return (new_points,sampled_xyz)
 
