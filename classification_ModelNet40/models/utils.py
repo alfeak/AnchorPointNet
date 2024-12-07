7b104c070524bc459efdd6d39986b3c7e38c06d6
alfeak <lensurplus@gmail.com>
2024-12-07 14:09:24 +0000

    new_baseline

--- classification_ModelNet40/models/utils.py
+++ classification_ModelNet40/models/utils.py
@@ -127,10 +127,6 @@ class PointConv(nn.Module):
           nn.MaxPool2d((1,knn)),
         )
         self.bn1 = nn.BatchNorm1d(out_channel)
-        self.identity = nn.Sequential(
-          nn.Conv1d(in_channel,out_channel,kernel_size=1,bias=False),
-          nn.BatchNorm1d(out_channel),
-        )
         
     def forward(self,x):
         points,xyz = x
@@ -147,7 +143,7 @@ class PointConv(nn.Module):
         grouped_points = self.bn(grouped_points) + sampled_points.unsqueeze(-1)
         grouped_points = self.conv(grouped_points).squeeze(-1)
         grouped_points = self.bn1(grouped_points)
-        new_points = F.relu(grouped_points + self.identity(sampled_points))
+        new_points = F.relu(grouped_points)
 
         return (new_points,sampled_xyz)
 
@@ -166,25 +162,24 @@ class PointResConv(nn.Module):
         )
         self.bn1d = nn.BatchNorm1d(out_channel)
 
-        self.bn2d1 = nn.BatchNorm2d(out_channel)
         self.conv1 = nn.Sequential(
-            nn.Conv2d(out_channel,out_channel,kernel_size=1,bias=False),
-            nn.MaxPool2d((1,knn)),
+            nn.Conv1d(out_channel,out_channel,kernel_size=1,bias=False),
+            nn.BatchNorm1d(out_channel),
         )
-        self.bn1d1 = nn.BatchNorm1d(out_channel)
-        # if in_channel == out_channel:
-        #     self.identity = nn.Sequential()
-        # else:
-        #     self.identity = nn.Sequential(
-        #         nn.Conv1d(in_channel,out_channel,kernel_size=1,bias=False),
-        #         nn.BatchNorm1d(out_channel),
-        #     )
+        if in_channel == out_channel:
+            self.identity = nn.Sequential()
+        else:
+            self.identity = nn.Sequential(
+                nn.Conv1d(in_channel,out_channel,kernel_size=1,bias=False),
+                nn.BatchNorm1d(out_channel),
+            )
         
     def forward(self,x):
         points,xyz = x
         B, N, C = xyz.shape
         xyz = xyz.contiguous() 
         fps_idx = pointnet2_utils.furthest_point_sample(xyz, N//self.stride).long()
+        # fps_idx = sort_sample(xyz,self.stride)
         sampled_xyz = index_points(xyz, fps_idx)
         sampled_points = index_points(points.permute(0,2,1),fps_idx).permute(0,2,1)
         
@@ -195,14 +190,7 @@ class PointResConv(nn.Module):
         grouped_points = self.conv(grouped_points).squeeze(-1)
         grouped_points = self.bn1d(grouped_points)
         new_points = F.relu(grouped_points)
-        
-        idx = knn_point(self.knn * self.dilation, sampled_xyz, sampled_xyz)[:, :, ::self.dilation]
-        grouped_points = index_points(new_points.permute(0,2,1), idx)
-        grouped_points = grouped_points.permute(0,3,1,2)
-        grouped_points = self.bn2d1(grouped_points) + new_points.unsqueeze(-1)
-        grouped_points = self.conv1(grouped_points).squeeze(-1)
-        grouped_points = self.bn1d1(grouped_points)
-        new_points = F.relu(grouped_points)
+        new_points = F.relu(self.conv1(new_points) + self.identity(sampled_points))
 
         return (new_points,sampled_xyz)
 
