15ea7ed33232ad98c1223ccd05ac15c2328e4fa5
alfeak <lensurplus@gmail.com>
2024-12-07 18:15:33 +0000

    new_baseline

--- classification_ModelNet40/models/utils.py
+++ classification_ModelNet40/models/utils.py
@@ -157,15 +157,15 @@ class PointResConv(nn.Module):
         self.out_channel = out_channel
         self.bn2d = nn.BatchNorm2d(in_channel)
         self.conv = nn.Sequential(
-            nn.Conv2d(in_channel,out_channel,kernel_size=1,bias=False),
+            nn.Conv2d(in_channel*2,out_channel,kernel_size=1,bias=False),
             nn.MaxPool2d((1,knn)),
         )
         self.bn1d = nn.BatchNorm1d(out_channel)
 
-        self.conv1 = nn.Sequential(
-            nn.Conv1d(out_channel,out_channel,kernel_size=1,bias=False),
-            nn.BatchNorm1d(out_channel),
-        )
+        # self.conv1 = nn.Sequential(
+        #     nn.Conv1d(out_channel,out_channel,kernel_size=1,bias=False),
+        #     nn.BatchNorm1d(out_channel),
+        # )
         if in_channel == out_channel:
             self.identity = nn.Sequential()
         else:
@@ -186,11 +186,14 @@ class PointResConv(nn.Module):
         idx = knn_point(self.knn * self.dilation, xyz, sampled_xyz)[:, :, ::self.dilation]
         grouped_points = index_points(points.permute(0,2,1), idx)
         grouped_points = grouped_points.permute(0,3,1,2)
-        grouped_points = self.bn2d(grouped_points) + sampled_points.unsqueeze(-1)
+        # grouped_points = self.bn2d(grouped_points) + sampled_points.unsqueeze(-1)
+        grouped_points = self.bn2d(grouped_points)
+        grouped_points = torch.cat([grouped_points,sampled_points.unsqueeze(-1).repeat(1,1,1,self.knn)],dim=1)
         grouped_points = self.conv(grouped_points).squeeze(-1)
         grouped_points = self.bn1d(grouped_points)
-        new_points = F.relu(grouped_points)
-        new_points = F.relu(self.conv1(new_points) + self.identity(sampled_points))
+        # new_points = F.relu(grouped_points)
+        # new_points = F.relu(self.conv1(new_points) + self.identity(sampled_points))
+        new_points = F.relu(grouped_points + self.identity(sampled_points))
 
         return (new_points,sampled_xyz)
 
