1a5369c0fd90dd270b1936508c8c7d94716ecdf3
alfeak <lensurplus@gmail.com>
2024-12-10 13:34:42 +0000

    new_baseline_sort

--- classification_ModelNet40/models/utils.py
+++ classification_ModelNet40/models/utils.py
@@ -132,12 +132,12 @@ class PointConv(nn.Module):
         grouped_points = self.bn(grouped_points)
         new_points = F.relu(grouped_points + self.identity(sampled_points.permute(0,2,1)))
 
-        idx = knn_point(self.knn, sampled_xyz, sampled_xyz)
-        grouped_points = index_points(new_points.permute(0,2,1), idx)
-        grouped_points = grouped_points.permute(0,3,1,2)
-        grouped_points = self.conv1(grouped_points).squeeze(-1)
-        grouped_points = self.bn1(grouped_points)
-        new_points = F.relu(grouped_points + new_points)
+        # idx = knn_point(self.knn, sampled_xyz, sampled_xyz)
+        # grouped_points = index_points(new_points.permute(0,2,1), idx)
+        # grouped_points = grouped_points.permute(0,3,1,2)
+        # grouped_points = self.conv1(grouped_points).squeeze(-1)
+        # grouped_points = self.bn1(grouped_points)
+        # new_points = F.relu(grouped_points + new_points)
 
         return (new_points,sampled_xyz)
         
